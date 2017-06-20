35dc730d42f125bf5dc6e3ca49842f93ef4c468b
Sanders <CLS4@sedgwicklaw.com>
2017-06-20 10:39:55 -0500

    new global config setting

--- Eicm.Api/Global.asax.cs
+++ Eicm.Api/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using System.Web;
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Newtonsoft.Json;
 
 namespace Eicm.Api
 {
@@ -12,10 +15,13 @@ namespace Eicm.Api
             AreaRegistration.RegisterAllAreas();
 
             Bootstrapper.Run();
-            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
-                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters
-                .XmlFormatter);
+            //GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
+            //    Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            //GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters
+            //    .XmlFormatter);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+                .Add(new RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase, true, "application/json"));
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             
